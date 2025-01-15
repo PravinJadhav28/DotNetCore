39359161fd286ab3bda2e513b428bc5abdda7054
lenovo <lenovo@LAPTOP-RD6BG50C>
2025-01-15 23:53:09 +0530

    Introduction of Dot Net Core

---
+++ B24MathLibrary/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace B24MathLibrary
+{
+    public class Calculator
+    {
+
+        public void Add(int a,int b)
+        {
+            Console.WriteLine($"Addition :{a +  b}");   
+        }
+
+    }
+}
---
+++ MathClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using B24MathLibrary;
+using System;
+
+namespace MathClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            c.Add(10, 10);  
+            
+            Console.ReadKey();
+        }
+    }
+}
